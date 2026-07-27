fc06c8e2210390f3446e2750aea5c1c1b2100900
Mahfuzur Rahman <mxhfxzur.r@gmail.com>
2026-07-27 10:51:48 +0100

    Add second test

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -25,5 +25,24 @@ public class ASimplePlaywrightTest {
 
     }
 
+    @Test
+    void shouldSearchByKeyword() {
+
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+
+        page.navigate("https://practicesoftwaretesting.com");
+        page.locator("[placeholder=Search]").fill("Pliers");
+        page.locator("button:has-text('Search')").click();
+
+        int matchingSearchResults = page.locator(".card").count();
+
+        Assertions.assertTrue(matchingSearchResults > 0);
+
+        browser.close();
+        playwright.close();
+
+    }
 
 }
