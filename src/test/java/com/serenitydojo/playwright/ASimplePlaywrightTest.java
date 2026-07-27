a0872763414d594ef637b0e67083a918a9449c13
Mahfuzur Rahman <mxhfxzur.r@gmail.com>
2026-07-27 10:57:02 +0100

    Add playwright annotation for clean code

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -3,34 +3,27 @@ package com.serenitydojo.playwright;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+@UsePlaywright
 public class ASimplePlaywrightTest {
 
     @Test
-    void shouldShowThePageTitle() {
-
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
+    void shouldShowThePageTitle(Page page) {
 
         page.navigate("https://practicesoftwaretesting.com");
         String title = page.title();
 
         Assertions.assertTrue(title.contains("Practice Software Testing"));
 
-        browser.close();
-        playwright.close();
-
     }
 
     @Test
-    void shouldSearchByKeyword() {
-
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
+    void shouldSearchByKeyword(Page page) {
 
         page.navigate("https://practicesoftwaretesting.com");
         page.locator("[placeholder=Search]").fill("Pliers");
@@ -39,9 +32,6 @@ public class ASimplePlaywrightTest {
         int matchingSearchResults = page.locator(".card").count();
 
         Assertions.assertTrue(matchingSearchResults > 0);
-
-        browser.close();
-        playwright.close();
 
     }
 
