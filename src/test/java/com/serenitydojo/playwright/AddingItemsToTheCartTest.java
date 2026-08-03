040b1a4a9b320cf203ab26433685ee0d495703f8
Mahfuzur Rahman <mxhfxzur.r@gmail.com>
2026-08-03 14:43:06 +0100

    Add test case

--- src/test/java/com/serenitydojo/playwright/AddingItemsToTheCartTest.java
+++ src/test/java/com/serenitydojo/playwright/AddingItemsToTheCartTest.java
@@ -2,7 +2,9 @@ package com.serenitydojo.playwright;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import com.microsoft.playwright.junit.UsePlaywright;
+import com.microsoft.playwright.options.AriaRole;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,25 +23,20 @@ public class AddingItemsToTheCartTest {
 
         page.navigate("https://practicesoftwaretesting.com");
         page.getByPlaceholder("Search").fill("Pliers");
-        page.getByPlaceholder("Search").press("Enter");
-
-        page.waitForLoadState();
-        page.waitForCondition(() -> page.getByTestId("product-name").count() > 0);
-
-        List<String> products = page.getByTestId("product-name").allTextContents();
-        Assertions.assertThat(products.get(0)).containsIgnoringCase("Pliers");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Search")).click();
 
         assertThat(page.locator(".card")).hasCount(4);
 
         List<String> productNames = page.getByTestId("product-name").allTextContents();
         Assertions.assertThat(productNames).allMatch(name -> name.contains("Pliers"));
 
-        Locator outOfStockItem = page.locator(".card")
+        List<String> outOfStockItems = page.locator(".card")
                 .filter(new Locator.FilterOptions().setHasText("Out of stock"))
-                .getByTestId("product-name");
+                .getByTestId("product-name").allTextContents();
 
-        assertThat(outOfStockItem).hasCount(1);
-        assertThat(outOfStockItem).hasText("Long Nose Pliers");
+        Assertions.assertThat(outOfStockItems).hasSize(2);
+        Assertions.assertThat(outOfStockItems).contains(" Long Nose Pliers ", " Combination Pliers ");
+
     }
 
 }
